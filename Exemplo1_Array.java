f47285edf5424fb2bd06f121a5ee41d3b74b9a5a
Iran <113207304+iranCosta@users.noreply.github.com>
2025-06-26 10:53:56 -0300

    Update Exemplo1_Array.java
    Adição de um sysout

--- Exemplo1_Array.java
+++ Exemplo1_Array.java
@@ -11,6 +11,8 @@ public class Exemplo1_Array {
 		int A[] = new int[n];
 		int B[] = new int[n];
 
+		System.out.println("Alterado via github")
+	
 		for (int i = 0; i < n; i++) {
 			System.out.println("Entre com um valor da posiçao: ");
 			A[i] = scan.nextInt();
