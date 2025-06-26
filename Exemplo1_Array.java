3e49524398e202bfdff4ed0c136aebbe7c7f4661
Iran <iranandrei76@gmail.com>
2025-06-26 09:52:39 -0300

    Primeiro commmit

---
+++ Exemplo1_Array.java
@@ -0,0 +1,32 @@
+package exemplos_arrays;
+
+import java.util.Scanner;
+
+public class Exemplo1_Array {
+
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+
+		int n = 5;
+		int A[] = new int[n];
+		int B[] = new int[n];
+
+		for (int i = 0; i < n; i++) {
+			System.out.println("Entre com um valor da posiçao: ");
+			A[i] = scan.nextInt();
+			B[i] = A[i];
+
+		}
+
+		System.out.println("VetorA ");
+		for (int i = 0; i < A.length; i++) {
+			System.out.println(A[i] + " ");
+		}
+		System.out.println("VetorB ");
+		for (int i = 0; i < B.length; i++) {
+			System.out.println(B[i] + " ");
+
+		}
+	}
+
+}
